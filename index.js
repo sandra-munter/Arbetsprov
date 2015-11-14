082d695da1e45a6c26d56e3b5a08144b8414da76
alexandra <alexandra@vinnovera.se>
2015-11-14 11:35:15 +0100

    server

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+	app     = express(),
+	path    = require("path");
+
+	app.get('/',function(req,res){
+		res.sendFile(path.join(__dirname+'/html/index.html'));
+	});
+
+	app.use("/css", express.static(__dirname + '/css'));
+	app.use("/js", express.static(__dirname + '/js'));
+
+
+	app.listen(8089);
+
